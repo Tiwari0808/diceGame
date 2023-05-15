95ce2fcb0d51b44332a23ac95b3c615f842a1a36
Divyanshu <Divyanshutiwari173@gmail.com>
2023-05-16 00:41:48 +0530

    secondimg

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -6,3 +6,10 @@ let randomImageSource = "images/" + randomDiceImage;
 
 let image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
+
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+let randomImageSource2 ="images/dice"+randomNumber2+".png";
+
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
