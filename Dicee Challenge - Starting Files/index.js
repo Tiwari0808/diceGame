fe7bd44f4e871d45b5a70284efa06b3dbce424ad
Divyanshu <Divyanshutiwari173@gmail.com>
2023-05-16 02:50:41 +0530

    finalDiceProject

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -13,3 +13,11 @@ let randomImageSource2 ="images/dice"+randomNumber2+".png";
 
 let image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Play 1 Wins!";
+} else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Play 2 Wins!";
+} else{
+    document.querySelector("h1").innerHTML="Draw";
+}
